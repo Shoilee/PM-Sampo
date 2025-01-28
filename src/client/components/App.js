2d3705ba30122d60e0007fa162a3498b2df2a06c
Shoilee <sarahshoilee121@gmail.com>
2025-01-28 14:38:30 +0200

    update calendar in English

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import AdapterMoment from '@mui/lab/AdapterMoment'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
-import 'moment/locale/fi'
+/* import calander in different language */
+/* import 'moment/locale/fi' */
+/* import 'moment/locale/en-gb' */
 import SemanticPortal from '../containers/SemanticPortal'
 import portalConfig from '../../configs/portalConfig.json'
 

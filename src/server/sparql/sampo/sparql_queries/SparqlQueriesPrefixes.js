6ffb418d32407cf717940a6ea9f054a8666376da
Shoilee <sarahshoilee121@gmail.com>
2025-01-28 14:28:41 +0200

    fix historical events name and add place of origin hierarchy

--- src/server/sparql/sampo/sparql_queries/SparqlQueriesPrefixes.js
+++ src/server/sparql/sampo/sparql_queries/SparqlQueriesPrefixes.js
@@ -10,14 +10,10 @@ export const prefixes = `
   PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
   PREFIX sch: <http://schema.org/>
   PREFIX geosparql: <http://www.opengis.net/ont/geosparql#>
-  PREFIX frbroo: <http://erlangen-crm.org/efrbroo/>
-  PREFIX mmm-schema: <http://ldf.fi/schema/mmm/>
   PREFIX gvp: <http://vocab.getty.edu/ontology#>
-  PREFIX wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#>
   PREFIX spatial: <http://jena.apache.org/spatial#>
   PREFIX sd: <http://www.w3.org/ns/sparql-service-description#>
   PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
-  PREFIX h-schema: <http://ldf.fi/schema/hellerau/> 
   PREFIX gn: <http://www.geonames.org/ontology#>
-  PREFIX semparls: <http://ldf.fi/schema/semparl/>
+  PREFIX aat: <http://vocab.getty.edu/aat/>
 `

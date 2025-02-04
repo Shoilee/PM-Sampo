adb05f77f24c4d68417db9f41e871639a1181179
Shoilee <sarahshoilee121@gmail.com>
2025-02-04 20:15:22 +0200

    update perspective uri

--- src/server/sparql/sampo/sparql_queries/SparqlQueriesActors.js
+++ src/server/sparql/sampo/sparql_queries/SparqlQueriesActors.js
@@ -1,8 +1,10 @@
+const perspectiveID = 'actors'
+
 export const actorProperties = `
     {
       ?id rdfs:label ?prefLabel__id .
       BIND(STR(?prefLabel__id) AS ?prefLabel__prefLabel)
-      BIND(CONCAT("/actors/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
       BIND(?id as ?uri__id)
       BIND(?id as ?uri__dataProviderUrl)
       BIND(?id as ?uri__prefLabel)

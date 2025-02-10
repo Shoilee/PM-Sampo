114079742a7fd50d958d78074824cab20d213d6f
Shoilee <sarahshoilee121@gmail.com>
2025-02-10 21:34:13 +0200

    add `new` related collection tab for histEvents

--- src/server/sparql/sampo/sparql_queries/SparqlQueriesHistEvents.js
+++ src/server/sparql/sampo/sparql_queries/SparqlQueriesHistEvents.js
@@ -25,17 +25,46 @@ export const histEventProperties =
   }
 `
 
-export const knowledgeGraphMetadataQuery = `
-  SELECT * 
+export const histEventCollectionsQuery = `
+  SELECT DISTINCT ?object__id ?id ?uri__id ?uri__prefLabel ?uri__dataProviderUrl ?prefLabel__id  
+  ?object__prefLabel__id ?object__prefLabel__prefLabel ?object__prefLabel__dataProviderUrl
+  ?object__uri__id ?object__uri__prefLabel ?object__uri__dataProviderUrl
+  ?object__productionPlace__id ?object__productionPlace__prefLabel
+  ?object__productionTimeSpan__id ?object__productionTimeSpan__prefLabel
+  ?object__image__id ?object__image__url
   WHERE {
-    ?id a sd:Dataset ;
-        dct:title ?title ;
-        dct:publisher ?publisher ;
-        dct:rightsHolder ?rightsHolder ;
-        dct:modified ?modified ;
-        dct:source ?databaseDump__id .
-    ?databaseDump__id skos:prefLabel ?databaseDump__prefLabel ;
-                      mmm-schema:data_provider_url ?databaseDump__dataProviderUrl ;
-                      dct:modified ?databaseDump__modified .
-  }
+    <FILTER>
+    BIND(<ID> as ?id)
+    BIND(?id as ?uri__id)
+    BIND(?id as ?uri__prefLabel)
+    BIND(?id as ?uri__dataProviderUrl)
+    ?object__id crm:P141i_was_assigned_by/crm:P141_assigned ?id .
+    BIND(?object__id AS ?object__uri__id)
+    BIND(?object__id AS ?object__uri__dataProviderUrl)
+    BIND(STR(?object__id) AS ?object__uri__prefLabel) 
+    {
+      ?object__id dct:title ?object__prefLabel__id .
+      BIND(?object__prefLabel__id AS ?object__prefLabel__prefLabel)
+      BIND(CONCAT("/collections/page/", REPLACE(STR(?object__id), "^.*\\\\/(.+)", "$1")) AS ?object__prefLabel__dataProviderUrl)
+    }
+    UNION
+    {
+      ?object__id crm:P108i_was_produced_by/crm:P7_took_place_at ?object__productionPlace__id .
+      ?object__productionPlace__id skos:prefLabel ?object__productionPlace__prefLabel .
+    }
+    UNION
+    {
+      ?object__id crm:P108i_was_produced_by/crm:P4_has_time-span ?object__productionTimeSpan__id.
+      ?object__productionTimeSpan__id crm:P82a_begin_of_the_begin ?object__productionTimeSpan__start .
+      ?object__productionTimeSpan__id crm:P82b_end_of_the_end ?object__productionTimeSpan__end .
+      BIND(CONCAT(STR(?object__productionTimeSpan__start), " --- " , STR(?object__productionTimeSpan__end) ) AS ?object__productionTimeSpan__prefLabel)
+    }
+    UNION
+    {
+      ?object__id crm:P65_shows_visual_item ?i_BNODE .
+      ?i_BNODE a crm:E36_Visual_Item .
+      ?i_BNODE <https://linked.art/ns/terms/digitally_shown_by> ?object__image__id .
+      ?object__image__id <https://linked.art/ns/terms/access_point> ?object__image__url .
+    }
+  }LIMIT 10
 `
